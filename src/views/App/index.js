55e738e299b400deb6baab41172c9cf0d1f53b92
Kim, Harim <harimkims@gmail.com>
2018-09-02 02:20:49 +0900

    아폴로 클라이언트 연동

--- src/views/App/index.js
+++ src/views/App/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import Navbar from '../../components/Navbar';
 import Contents from '../../components/Contents';
+
+const client = new ApolloClient({
+  uri: 'https://sauna-graphql.herokuapp.com/'
+});
 
 class App extends React.Component {
   render() {
     return (
-      <Wrapper>
-        <Navbar />
-        <Contents>
-          Hello
-        </Contents>
-      </Wrapper>
+      <ApolloProvider client={client}>
+        <Wrapper>
+          <Navbar />
+          <Contents>
+            Hello
+          </Contents>
+        </Wrapper>
+      </ApolloProvider>
     );
   }
 }
