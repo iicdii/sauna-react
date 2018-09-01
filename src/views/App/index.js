245981800d34da6682f1270336380fa6cfb63a53
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-09-02 01:12:11 +0900

    imported Navbar & Contents compo

--- src/views/App/index.js
+++ src/views/App/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+import Navbar from '../../components/Navbar';
+import Contents from '../../components/Contents';
 
 class App extends React.Component {
   render() {
     return (
-      <div>Hello World!</div>
+      <Wrapper>
+        <Navbar />
+        <Contents>
+          Hello
+        </Contents>
+      </Wrapper>
     );
   }
 }
+
+const Wrapper = styled.div``;
 
 export default App;
